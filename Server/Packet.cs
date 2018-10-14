5dc15bc653cdd0b9449672c8ae5c691392890a4a
Dario Salza <dsalza@hotmail.it>
2018-10-14 15:54:00 +0200

    Skelethon

---
+++ Server/Packet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Packet
+    {
+        public class Reception
+        {
+            Station ReceivingStation;
+            double RSSI;
+        }
+        List<Reception> Receivings = new List<Reception>();
+        String SendingMAC;
+        String RequestedSSID;
+        DateTime Timestamp;
+        String Hash;
+    }
+}
---
+++ Server/Station.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Station
+    {
+        Position p;
+        String NameMAC;
+        DateTime lastHearthbeat;
+        DateTime addedAt;
+        public enum Status {Alive,Zombie};
+        Status stat;
+    }
+}
