4727fdba05e34aee9e18dcc521ae3854d3edfcaa
iFederx <federico.brignone@gmail.com>
2018-10-20 17:29:46 +0200

    master update
    copiato il server da master

---
+++ Server/AnalysisEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class AnalysisEngine
+    {
+        BlockingCollection<Packet> AnalysisQueue = new BlockingCollection<Packet>();
+        public class AnalysisResult
+        {
+
+        }
+        /// <summary>
+        /// Lanciare questo metodo ogni qual volta un pacchetto è "maturo" (ovvero ricevuto da tutte le stazioni o andato in timeout).
+        /// </summary>
+        /// <param name="p">Pacchetto da analizzare</param>
+        /// <param name="c">Contesto</param>
+        public void sendToAnalysisQueue(Packet p)
+        {
+            AnalysisQueue.Add(p);
+        }
+    }
+}
---
+++ Server/Context.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Context
+    {
+        Dictionary<String,Station> stations = new Dictionary<String,Station>();
+        AnalysisEngine analysEngine;
+        public Context(AnalysisEngine anEngine)
+        {
+            analysEngine = anEngine;
+        }
+        ArrayList numberOfAliveStationsPerRoom = new ArrayList();
+        public void addStation(Station s)
+        {
+
+        }
+        public void getStation(String NameMAC)
+        {
+
+        }
+        public void checkStationAliveness()
+        {
+
+        }
+
+
+    }
+}
