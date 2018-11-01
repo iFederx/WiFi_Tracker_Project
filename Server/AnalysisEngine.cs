e967b28537222453d05ef4ab105042702f2f6a45
Dario Salza <dsalza@hotmail.it>
2018-11-01 15:49:31 +0100

    Some additions and refactoring to AnalysisEngine

--- Server/AnalysisEngine.cs
+++ Server/AnalysisEngine.cs
@@ -15,10 +15,6 @@ namespace Server
         ConcurrentDictionaryStack<String, Device> deviceMap = new ConcurrentDictionaryStack<String, Device>();
         volatile bool killed = false;
         int anPack = 0;
-        public class AnalysisResult
-        {
-
-        }
         /// <summary>
         /// Lanciare questo metodo ogni qual volta un pacchetto è "maturo" (ovvero ricevuto da tutte le stazioni o andato in timeout).
         /// </summary>
@@ -29,14 +25,48 @@ namespace Server
             AnalysisQueue.Enqueue(p);
             condVar.Set();            
         }
+        private void analyzerProcess()
+        {
+            while (!killed)
+            {
+                Packet p;
+                bool success;
+                try
+                {
+                    if (success = AnalysisQueue.TryDequeue(out p))
+                    {
+                        analyzePacket(p);
+                        anPack++;
+                    }
+                    if (!success || (anPack & 1024) == 0)
+                    {
+                        updateLongTerm();
+                        anPack = 0;
+                    }
+                    if (!success)
+                    {
+                        condVar.WaitOne();
+                    }
+                }
+                catch (ObjectDisposedException)//fatal
+                {
+                    killed = true;
+                }
+            }
+        }
         private void analyzePacket(Packet p)
         {
             Device d;
             bool anew;
+            //check aliases of anonymous, and if corresponds set p.sendingMAC all'alias con cui è registrato
             if (anew=!deviceMap.getKey(p.SendingMAC, out d))
             {
                 d = new Device();
                 d.MAC = p.SendingMAC;
+                if(isMACLocal(p.SendingMAC))
+                {
+                    d.anonymous = true;
+                }
                 d.firstSeen = p.Timestamp;
             }
             d.lastSeen = p.Timestamp;
@@ -48,9 +78,25 @@ namespace Server
             triangulate(d, p);
             if (anew)
             {
+                if (d.anonymous)
+                    d = coalesce(d);
                 deviceMap.upsert(d.MAC, d,(old,cur)=> { return cur; });//single thread safe only
             }
         }
+
+        private Device coalesce(Device d)
+        {
+            //try to see if the Device has a previous version of itself, and return the old version, adding the new fields and the new mac to the aliases
+            //add alias to the table
+            //if impossible to fuse, generate
+            throw new NotImplementedException();
+        }
+
+        private bool isMACLocal(string sendingMAC)
+        {
+            throw new NotImplementedException();
+        }
+
         private void triangulate(Device d,Packet p)
         { }
         public void kill()
@@ -58,35 +104,7 @@ namespace Server
             killed = true;
             condVar.Dispose();
         }
-        private void analyzerProcess()
-        {
-            while(!killed)
-            {
-                Packet p;
-                bool success;
-                try
-                {
-                    if (success=AnalysisQueue.TryDequeue(out p))
-                    {
-                        analyzePacket(p);
-                        anPack++;
-                    }
-                    if(!success||(anPack&1024)==0)
-                    {
-                        updateLongTerm();
-                        anPack = 0;
-                    }
-                    if(!success)
-                    {
-                        condVar.WaitOne();
-                    }
-                }
-               catch(ObjectDisposedException)//fatal
-                {
-                    killed = true;
-                }
-            }           
-        }
+        
         private void updateLongTerm()
         {
 
